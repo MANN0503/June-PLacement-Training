2cedcd3dad40f2aa515b8c7de9fb91cef6bc57ea
Manbir Kaur <121239226+MANN0503@users.noreply.github.com>
2023-06-28 11:41:58 +0530

    Add files via upload
    Write a code to find the middle node of a linked list.

---
+++ MiddleNode.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+    public Node findMiddleNode() {
+        if(head == null)
+            return null;
+        
+        Node slowPointer = head;
+        Node fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer;
+        
+    }
+    
+}
+
+public class MiddleNode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList linkedList = new LinkedList();
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            int element = scanner.nextInt();
+            linkedList.insert(element);
+        }
+
+        Node middleNode = linkedList.findMiddleNode();
+        if (middleNode != null) {
+            System.out.println("Middle node value: " + middleNode.data);
+        } else {
+            System.out.println("List is empty.");
+        }
+    }
+}
